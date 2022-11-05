32e94ae0d16721cfec208d88e0b91a2ef6eda3e5
Marcelo Araujo <jeffmcbrain@gmail.com>
2022-11-05 19:07:10 -0300

    Connect to database and change support for ES6

--- web/server.js
+++ web/server.js
@@ -1,14 +1,15 @@
-const os = require('os');
-const express = require('express');
+import os from "os";
+import express from "express";
+import redis from "redis";
+import { sequelize } from "./db/models/index.js";
 const app = express();
-const redis = require('redis');
 const redisClient = redis.createClient({
   host: 'redis',
   port: 6379
 });
 
 app.get('/', function(req, res) {
-    redisClient.get('numVisits', function(err, numVisits) {
+    /*redisClient.get('numVisits', function(err, numVisits) {
         numVisitsToDisplay = parseInt(numVisits) + 1;
         if (isNaN(numVisitsToDisplay)) {
             numVisitsToDisplay = 1;
@@ -16,7 +17,13 @@ app.get('/', function(req, res) {
        res.send(os.hostname() +': Numberd of visits is at nodemon legacy: ' + numVisitsToDisplay);
         numVisits++;
         redisClient.set('numVisits', numVisits);
-    });
+    });*/
+    try {
+      sequelize.authenticate();
+      console.log('Connection has been established successfully.');
+    } catch (error) {
+      console.error('Unable to connect to the database:', error);
+    }
 });
 
 app.listen(5000, function() {
