74fb0549fa0c3edb06149e338230882da1ff0ef2
Marcelo Araujo <marcelojeffaraujo@gmail.com>
2023-03-23 08:33:28 -0300

    Adjust env vars for DB connection

--- web/config/db.js
+++ web/config/db.js
@@ -5,7 +5,7 @@ const dbConfig = {
   "development": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
+    "database": process.env.DB_DBNAME,
     "host": process.env.DB_HOST,
     "dialect": process.env.DB_DIALECT
   },
